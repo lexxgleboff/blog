b8ecc8cfdf2c11c2b8cfa4be5f1d5daf5de5e98e
Alex Glebov <lexxgleboff@gmail.com>
2023-01-31 13:47:06 +0300

    Исправил баг с описанием

--- src/utils/limitString.ts
+++ src/utils/limitString.ts
@@ -1,5 +1,5 @@
 function limitString(str: string, num = 200) {
-  return str.length > num ? `${str.slice(0, num)}...` : str
+  return str?.length > num ? `${str.slice(0, num)}...` : str
 }
 
 export default limitString
